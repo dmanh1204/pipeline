738f45550c08dc8c0fd1407f447d283acc978982
noobmdev <noobmdev@gmail.com>
2023-07-10 11:06:24 +0700

    test airflow dag in diff k8s namespace

--- dags/pi_spark_job_ns_airflow.py
+++ dags/pi_spark_job_ns_airflow.py
@@ -83,7 +83,7 @@ spark_pi = KubernetesPodOperator(
               "--conf",
               "spark.hadoop.fs.s3a.secret.key=zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
               "--conf",
-              "spark.hadoop.fs.s3a.endpoint='play.min.io:9000'",
+              "spark.hadoop.fs.s3a.endpoint=play.min.io:9000",
               "--conf",
               "spark.serializer=org.apache.spark.serializer.KryoSerializer",
               "--conf",
